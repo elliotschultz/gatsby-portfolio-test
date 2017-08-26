b8d52adef82e5fbe43a73cacda09308118e70989
Elliot Schultz <elliot@elliotschultz.com>
2017-08-26 22:16:19 +1000

    Got SSR working with styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
-    `gatsby-transformer-sharp`
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`
   ]
 }
